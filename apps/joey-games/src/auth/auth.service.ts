a7632b892ac1541839c45aab3e41d4b91fe984d7
Jovan <jovanmedford@gmail.com>
2024-05-03 09:12:01 -0400

    Feat: implement basic signup - add user to db

--- apps/joey-games/src/auth/auth.service.ts
+++ apps/joey-games/src/auth/auth.service.ts
@@ -1,14 +1,39 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { LoginDto, SignupDto } from './dto';
-import { userExistsMessage } from '../lib/constants';
+import { emailExistsMessage, userExistsMessage } from '../lib/constants';
+import { PrismaService } from '../prisma/prisma.service';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
   login(loginInfo: LoginDto) {
     return loginInfo;
   }
 
-  signup(signupInfo: SignupDto) {
-    return signupInfo;
+  async signup(signupInfo: SignupDto) {
+    signupInfo.password = await argon.hash(signupInfo.password);
+    try {
+      let user = await this.prisma.user.create({
+        data: signupInfo,
+        select: { email: true, username: true },
+      });
+      return user;
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          if (e.meta.target[0].includes('email')) {
+            throw new ConflictException(emailExistsMessage);
+          }
+
+          if (e.meta.target[0].includes('username')) {
+            throw new ConflictException(userExistsMessage);
+          }
+        }
+      }
+      throw e;
+    }
   }
 }
