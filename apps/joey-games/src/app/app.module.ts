ff98c44eabeda6432dade7a4a12c6333e61d8c67
Jovan <jovanmedford@gmail.com>
2024-08-30 12:46:06 -0400

    Feat: add multiplayer gateway to module

--- apps/joey-games/src/app/app.module.ts
+++ apps/joey-games/src/app/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { GameMetaModule } from '../game-meta/game-meta.module';
+import { MultiplayerGatewayGateway } from '../gateway/multiplayer-gateway.gateway';
 
 @Module({
-  imports: [AuthModule, PrismaModule, GameMetaModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    GameMetaModule,
+    MultiplayerGatewayGateway,
+  ],
 })
 export class AppModule {}
