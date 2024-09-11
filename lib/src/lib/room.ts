6c72536c384f6b8833b431a3a12db5c094a29c86
Jovan <jovanmedford@gmail.com>
2024-09-11 04:44:23 -0400

    Fix: change room to a class

--- lib/src/lib/room.ts
+++ lib/src/lib/room.ts
@@ -1,14 +1,22 @@
-import { UserDto } from "./dtos";
+import { UserDto } from './dtos';
+import { v4 as uuidv4 } from 'uuid';
 
-export interface Room {
+export class Room {
   id: string;
   currentActivity: Activity;
   host: string;
   players: Map<string, PlayerData>;
+
+  constructor(host: string) {
+    this.id = uuidv4();
+    this.currentActivity = 'lobby';
+    this.host = host;
+    this.players = new Map<string, PlayerData>();
+  }
 }
 
 export interface PlayerData extends UserDto {
-  status: PlayerStatus
+  status: PlayerStatus;
 }
 export type PlayerStatus = 'pending' | 'disconnected' | 'declined' | 'ready';
 export type Activity = 'lobby' | 'memoryGame';
