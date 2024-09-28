66eb9877b8ba5e2fdc4f4347edcc6cd74dc3a067
Jovan <jovanmedford@gmail.com>
2024-09-28 08:40:30 -0400

    Feat: add invitation controller and cleanup invitation service
    Add try catch to invitation service. Add pending to invitation controller.

--- apps/joey-games/src/invitation/invitation.controller.ts
+++ apps/joey-games/src/invitation/invitation.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { InvitationService } from './invitation.service';
+import { AuthenticatedRequest } from '@joey-games/lib';
 
 @Controller('invitation')
-export class InvitationController {}
+export class InvitationController {
+  constructor(private invitationService: InvitationService) {}
+
+  @Get('pending')
+  async findAll(@Req() request: AuthenticatedRequest) {
+    return await this.invitationService.findAllPendingInvitations(
+      request.session.user.email
+    );
+  }
+}
--- apps/joey-games/src/invitation/invitation.service.ts
+++ apps/joey-games/src/invitation/invitation.service.ts
@@ -1,47 +1,74 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { Invitation, InvitationStatus } from '@prisma/client';
+import { Invitation  } from '@prisma/client';
 import { InvitationReply } from '@joey-games/lib';
-import { Result } from '@joey-games/lib';
-import { createFailureResult, createSuccessResult } from '../lib/utils';
+
 
 @Injectable()
 export class InvitationService {
   constructor(private prisma: PrismaService) {}
 
   async createInvitation(to: string, roomId: string) {
-    let pendingInvitation = await this.findPendingInvitation(to, roomId);
-    if (pendingInvitation) return pendingInvitation;
-    
-    let invitation = await this.prisma.invitation.create({
-      data: {
-        roomId,
-        to,
-      },
-    });
-    return invitation;
+    try {
+      let pendingInvitation = await this.findPendingInvitation(to, roomId);
+      if (pendingInvitation) return pendingInvitation;
+
+      let invitation = await this.prisma.invitation.create({
+        data: {
+          roomId,
+          to,
+        },
+      });
+      return invitation;
+    } catch (e) {
+      console.log(e);
+    }
   }
 
-  async updateInvitationStatus({invitationId,  status}: InvitationReply)  {
-    let updatedInvitation = await this.prisma.invitation.update({
-      where: {
-        id: invitationId,
-      },
-      data: {
-        status,
-      },
-    });
-    return updatedInvitation;
+  async updateInvitationStatus({ invitationId, status }: InvitationReply) {
+    try {
+      let updatedInvitation = await this.prisma.invitation.update({
+        where: {
+          id: invitationId,
+        },
+        data: {
+          status,
+        },
+      });
+      return updatedInvitation;
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   async findPendingInvitation(to: string, roomId: string) {
-    let pendingInvitation: Invitation = await this.prisma.invitation.findFirst({
-      where: {
-        roomId,
-        to,
-        status: null,
-      },
-    });
-    return pendingInvitation
+    try {
+      let pendingInvitation: Invitation =
+        await this.prisma.invitation.findFirst({
+          where: {
+            roomId,
+            to,
+            status: null,
+          },
+        });
+      return pendingInvitation;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async findAllPendingInvitations(to: string) {
+    try {
+      let pendingInvitation: Invitation[] =
+        await this.prisma.invitation.findMany({
+          where: {
+            to,
+            status: null,
+          },
+        });
+      return pendingInvitation;
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
